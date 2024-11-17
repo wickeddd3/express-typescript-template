a74cf59cec7a8261028dca2d7fb232cd80714714
wickeddd3 <pablophilipandrew@gmail.com>
2024-11-18 04:17:36 +0800

    feat: add user schema

---
+++ src/schemas/user.schema.ts
@@ -0,0 +1,9 @@
+import { z } from "zod";
+
+export const userSchema = z.object({
+  email: z.string().email(),
+  name: z.string().max(50),
+  password: z.string().min(6),
+});
+
+export type UserSchemaType = z.infer<typeof userSchema>;
