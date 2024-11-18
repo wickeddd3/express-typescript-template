3731d23c0b3af13521f2d4770af042c4c87e9582
wickeddd3 <pablophilipandrew@gmail.com>
2024-11-19 03:05:29 +0800

    feat: add categories respository

---
+++ src/repositories/categories.repository.ts
@@ -0,0 +1,71 @@
+import { prisma } from "@/lib/prisma";
+import { CategorySchemaType } from "@/schemas/category.schema";
+import { LIST_QUERY } from "@/constants/list-query";
+import { ListQueryParams } from "@/types/query.type";
+import { Category } from "@prisma/client";
+
+export class CategoriesRepository {
+  private db = prisma;
+
+  public async list(query = LIST_QUERY): Promise<Category[] | Error> {
+    try {
+      const { orderBy, order, take, skip } = query as ListQueryParams;
+      const categories = await this.db.category.findMany({
+        orderBy: { [orderBy]: order },
+        take,
+        skip,
+      });
+      return categories as Category[];
+    } catch (error: any) {
+      throw new Error(error.message);
+    }
+  }
+
+  public async get(id: number): Promise<Category | Error> {
+    try {
+      const category = await this.db.category.findUnique({
+        where: { id },
+      });
+      return category as Category;
+    } catch (error: any) {
+      throw new Error(error.message);
+    }
+  }
+
+  public async create(data: CategorySchemaType): Promise<Category | Error> {
+    try {
+      const category = await this.db.category.create({
+        data: { ...data },
+      });
+      return category as Category;
+    } catch (error: any) {
+      throw new Error(error.message);
+    }
+  }
+
+  public async update(id: number, data: CategorySchemaType): Promise<Category | Error> {
+    try {
+      const category = await this.db.category.update({
+        where: { id },
+        data: {
+          ...data,
+        },
+      });
+      return category as Category;
+    } catch (error: any) {
+      throw new Error(error.message);
+    }
+  }
+
+  public async delete(id: number): Promise<void | Error> {
+    try {
+      await this.db.category.delete({
+        where: {
+          id,
+        },
+      });
+    } catch (error: any) {
+      throw new Error(error.message);
+    }
+  }
+}
