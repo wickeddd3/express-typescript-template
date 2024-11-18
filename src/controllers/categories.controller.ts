87506cb828edd625a1bc518f2e309e50fe86a233
wickeddd3 <pablophilipandrew@gmail.com>
2024-11-19 03:10:25 +0800

    feat: add categories controller swagger comment

--- src/controllers/categories.controller.ts
+++ src/controllers/categories.controller.ts
@@ -15,10 +15,145 @@ class CategoriesController implements Controller {
   }
 
   private initialiseRoutes(): void {
+    /**
+     * @swagger
+     * /api/categories:
+     *   get:
+     *     summary: Get all categories
+     *     tags: [Categories]
+     *     parameters:
+     *       - in: query
+     *         name: size
+     *         schema:
+     *           type: integer
+     *         description: Number of items per page
+     *       - in: query
+     *         name: page
+     *         schema:
+     *           type: integer
+     *         description: Page number
+     *       - in: query
+     *         name: orderBy
+     *         schema:
+     *           type: string
+     *         description: Field to sort by
+     *       - in: query
+     *         name: order
+     *         schema:
+     *           type: string
+     *           enum: [asc, desc]
+     *         description: Sort order
+     *     responses:
+     *       200:
+     *         description: List of categories
+     *         content:
+     *           application/json:
+     *             schema:
+     *               type: array
+     *               items:
+     *                 $ref: '#/components/schemas/Category'
+     *       500:
+     *         description: Server error
+     */
     this.router.get(`${this.path}`, this.list);
+    /**
+     * @swagger
+     * /api/categories:
+     *   post:
+     *     summary: Create a new category
+     *     tags: [Categories]
+     *     requestBody:
+     *       required: true
+     *       content:
+     *         application/json:
+     *           schema:
+     *             $ref: '#/components/schemas/Category'
+     *     responses:
+     *       201:
+     *         description: Category created successfully
+     *         content:
+     *           application/json:
+     *             schema:
+     *               $ref: '#/components/schemas/Category'
+     *       400:
+     *         description: Invalid input
+     */
     this.router.post(`${this.path}`, validationMiddleware(userSchema), this.create);
+    /**
+     * @swagger
+     * /api/categories/{id}:
+     *   get:
+     *     summary: Get a category by ID
+     *     tags: [Categories]
+     *     parameters:
+     *       - in: path
+     *         name: id
+     *         required: true
+     *         schema:
+     *           type: integer
+     *         description: Category ID
+     *     responses:
+     *       200:
+     *         description: Category details
+     *         content:
+     *           application/json:
+     *             schema:
+     *               $ref: '#/components/schemas/Category'
+     *       404:
+     *         description: Category not found
+     */
     this.router.get(`${this.path}/:id`, this.get);
+    /**
+     * @swagger
+     * /api/categories/{id}:
+     *   put:
+     *     summary: Update a category by ID
+     *     tags: [Categories]
+     *     parameters:
+     *       - in: path
+     *         name: id
+     *         required: true
+     *         schema:
+     *           type: integer
+     *         description: Category ID
+     *     requestBody:
+     *       required: true
+     *       content:
+     *         application/json:
+     *           schema:
+     *             $ref: '#/components/schemas/Category'
+     *     responses:
+     *       200:
+     *         description: Category updated successfully
+     *         content:
+     *           application/json:
+     *             schema:
+     *               $ref: '#/components/schemas/Category'
+     *       404:
+     *         description: Category not found
+     *       400:
+     *         description: Invalid input
+     */
     this.router.put(`${this.path}/:id`, validationMiddleware(userSchema), this.update);
+    /**
+     * @swagger
+     * /api/categories/{id}:
+     *   delete:
+     *     summary: Delete a category by ID
+     *     tags: [Categories]
+     *     parameters:
+     *       - in: path
+     *         name: id
+     *         required: true
+     *         schema:
+     *           type: integer
+     *         description: Category ID
+     *     responses:
+     *       200:
+     *         description: Category deleted successfully
+     *       404:
+     *         description: Category not found
+     */
     this.router.delete(`${this.path}/:id`, this.delete);
   }
 
