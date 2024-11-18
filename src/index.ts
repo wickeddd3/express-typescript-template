3778c111c6f6aa1fcfdc28878f3fcb7794c2f084
wickeddd3 <pablophilipandrew@gmail.com>
2024-11-19 03:06:44 +0800

    feat: pass categories controller to app

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,13 @@ import "module-alias/register";
 import { validateEnv } from "@/lib/validate-env";
 import App from "@/app";
 import ProductsController from "@/controllers/products.controller";
+import CategoriesController from "@/controllers/categories.controller";
 
 validateEnv();
 
-const app = new App([new ProductsController()], Number(process.env.PORT));
+const app = new App(
+  [new ProductsController(), new CategoriesController()],
+  Number(process.env.PORT)
+);
 
 app.listen();
