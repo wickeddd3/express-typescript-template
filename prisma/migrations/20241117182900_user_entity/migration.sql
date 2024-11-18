9d95cb932e2402f4599d922c715c8df48c1596dc
wickeddd3 <pablophilipandrew@gmail.com>
2024-11-19 04:12:13 +0800

    cut: remove migrations

--- prisma/migrations/20241117182900_user_entity/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Made the column `price` on table `products` required. This step will fail if there are existing NULL values in that column.
-
-*/
--- AlterTable
-ALTER TABLE `products` MODIFY `price` DECIMAL(65, 30) NOT NULL DEFAULT 0;
