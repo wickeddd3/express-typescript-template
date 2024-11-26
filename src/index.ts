6576993734114129a645e885e5e6537a6b422909
wickeddd3 <pablophilipandrew@gmail.com>
2024-11-26 22:25:13 +0800

    feat: add AuthController in main App

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,15 @@ import 'dotenv/config';
 import 'module-alias/register';
 import { validateEnv } from '@/lib/validate-env';
 import { App } from '@/app';
+import { AuthController } from '@/controllers/auth.controller';
 import { ProductsController } from '@/controllers/products.controller';
 import { CategoriesController } from '@/controllers/categories.controller';
 
 validateEnv();
 
-const app = new App([new ProductsController(), new CategoriesController()], Number(process.env.PORT));
+const app = new App(
+  [new AuthController(), new ProductsController(), new CategoriesController()],
+  Number(process.env.PORT),
+);
 
 app.listen();
