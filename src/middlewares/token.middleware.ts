34107b8106f127c581fe41d5c87184e6696be505
wickeddd3 <pablophilipandrew@gmail.com>
2024-11-26 22:17:21 +0800

    feat: add token middleware

---
+++ src/middlewares/token.middleware.ts
@@ -0,0 +1,26 @@
+import passport from 'passport';
+import { Strategy as JwtStrategy, ExtractJwt, VerifiedCallback } from 'passport-jwt';
+import { JWT_SECRET } from '@/config/jwt-options';
+import { prisma } from '@/lib/prisma';
+import Token from '@/interfaces/token.interface';
+
+const options = {
+  jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+  secretOrKey: JWT_SECRET,
+};
+
+const callback = async (payload: Token, done: VerifiedCallback) => {
+  const user = await prisma.user.findUnique({
+    where: { email: payload.email },
+  });
+  if (user) {
+    return done(null, user);
+  } else {
+    return done(null, false);
+  }
+};
+
+passport.use(new JwtStrategy(options, callback));
+
+export const initializePassport = () => passport.initialize();
+export const authenticateJwt = passport.authenticate('jwt', { session: false });
