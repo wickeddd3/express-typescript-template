23169d5f457204b8aab790ce3d304313b8ed905f
wickeddd3 <pablophilipandrew@gmail.com>
2024-12-01 02:16:08 +0800

    feat: add auth user api

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -5,6 +5,7 @@ import validationMiddleware from '@/middlewares/validation.middleware';
 import { userSchema } from '@/schemas/user.schema';
 import { loginSchema } from '@/schemas/login.schema';
 import { UsersService } from '@/services/users.service';
+import { authenticateJwt } from '@/middlewares/token.middleware';
 
 export class AuthController implements Controller {
   public path = '/auth';
@@ -18,6 +19,7 @@ export class AuthController implements Controller {
   private initialiseRoutes(): void {
     this.router.post(`${this.path}/register`, validationMiddleware(userSchema), this.register);
     this.router.post(`${this.path}/login`, validationMiddleware(loginSchema), this.login);
+    this.router.get(`${this.path}/me`, authenticateJwt, this.authUser);
   }
 
   private register = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
@@ -35,6 +37,14 @@ export class AuthController implements Controller {
       const { email, password } = req.body;
       const user = await this.usersService.authenticate({ email, password });
       res.status(200).json(user);
+    } catch (error: any) {
+      next(new HttpException(400, error.message));
+    }
+  };
+
+  private authUser = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+    try {
+      res.status(200).json(req.user);
     } catch (error: any) {
       next(new HttpException(400, error.message));
     }
