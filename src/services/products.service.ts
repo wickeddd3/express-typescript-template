1bdf305fce6219c827a3b9115474bc5da939fc16
wickeddd3 <pablophilipandrew@gmail.com>
2025-01-05 23:30:23 +0800

    feat: generate slug using product name

--- src/services/products.service.ts
+++ src/services/products.service.ts
@@ -1,6 +1,7 @@
 import { ProductsRepository } from '@/repositories/products.repository';
 import { ProductSchemaType } from '@/schemas/product.schema';
 import { ListQueryParams } from '@/types/query.type';
+import { createSlug } from '@/utils/slug';
 import { Product } from '@prisma/client';
 
 export class ProductsService {
@@ -27,7 +28,8 @@ export class ProductsService {
 
   public async create(data: ProductSchemaType): Promise<Product | Error> {
     try {
-      const product = await this.productsRepository.create(data);
+      const productWithSlug = { ...data, slug: createSlug(data.name) };
+      const product = await this.productsRepository.create(productWithSlug);
       return product as Product;
     } catch (error: any) {
       throw new Error(error.message);
